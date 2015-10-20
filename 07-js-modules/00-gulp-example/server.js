7974bb913e0877e10b5667e5bdb7764d986b90dc
Niels Henrik Juul Hansen <niha@eaaa.dk>
2015-10-20 07:44:52 +0200

    removed unnecessary files from Gulp example

--- 07-js-modules/00-gulp-example/server.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var app = express();
-app.use(express.static(__dirname + '/'));
-
-var server = app.listen(3000, function(){
-	var port = server.address().port;
-	console.log("Listening on port " + port);
-});
-
